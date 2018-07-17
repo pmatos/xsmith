efb18c78b3ee7b34c7bd5ee79aa2685c35718cae
William G Hatch <william@hatch.uno>
2018-07-17 13:40:17 -0600

    improve non-inheriting-rule definitions more

--- src/cish2-properties.rkt
+++ src/cish2-properties.rkt
@@ -17,35 +17,45 @@
   (define-syntax-class rule-type
     (pattern (~or (~datum choice-rule) (~datum ag-rule))))
   (syntax-parse stx
-    [(_ rt:rule-type name:id default-value:expr value-transformer:expr)
-     #'(define-property name
-         #:reads (grammar)
-         #:appends (rt name)
-         #:transformer
-         (λ (this-prop-info grammar-info)
-           (define rule-info
-             (for/hash ([node-name (dict-keys grammar-info)])
-               (define prop-vals
-                 (dict-ref this-prop-info node-name #f))
-               (values node-name
-                       (value-transformer
-                        (if prop-vals
-                            (syntax-parse prop-vals
-                              [(a) #'a]
-                              [(a b ...+)
-                               (raise-syntax-error
-                                #f
-                                (format
-                                 "duplicate definition of ~a property for node ~a."
-                                 'name
-                                 node-name)
-                                #'a)])
-                            (quote-syntax default-value))))))
-           (list rule-info)))]))
+    [(_ property-name:id
+        rt:rule-type
+        (~or
+         (~optional (~seq #:rule-name rule-name:id))
+         (~once (~seq #:default default-value:expr))
+         (~optional (~seq #:transformer value-transformer:expr))
+         )
+        ...)
+     (with-syntax ([transformer (or (attribute value-transformer) #'(λ (x) x))]
+                   [rule-name (or (attribute rule-name) #'property-name)])
+       #'(define-property property-name
+           #:reads (grammar)
+           #:appends (rt rule-name)
+           #:transformer
+           (λ (this-prop-info grammar-info)
+             (define rule-info
+               (for/hash ([node-name (dict-keys grammar-info)])
+                 (define prop-vals
+                   (dict-ref this-prop-info node-name #f))
+                 (values node-name
+                         (transformer
+                          (if prop-vals
+                              (syntax-parse prop-vals
+                                [(a) #'a]
+                                [(a b ...+)
+                                 (raise-syntax-error
+                                  #f
+                                  (format
+                                   "duplicate definition of ~a property for node ~a."
+                                   'property-name
+                                   node-name)
+                                  #'a)])
+                              (quote-syntax default-value))))))
+             (list rule-info))))]))
 
 (define-non-inheriting-rule-property
-  choice-rule
   may-be-generated
-  #t
-  (syntax-parser [#t #'(λ () this)]
-                 [#f #'(λ () #f)]))
+  choice-rule
+  #:default #t
+  #:transformer (syntax-parser [#t #'(λ () this)]
+                               [#f #'(λ () #f)]))
+
