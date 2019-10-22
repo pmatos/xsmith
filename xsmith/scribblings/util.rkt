99db2147ae69b64923470836e90fdcb218a190cb
Pierce Darragh <pierce.darragh@utah.edu>
2019-10-22 10:40:21 -0600

    Update description of command-line options.

--- xsmith/scribblings/util.rkt
+++ xsmith/scribblings/util.rkt
@@ -55,16 +55,18 @@
 
 (define (command-line-options-table)
   @itemlist[
-            @item{@verb{--help} -- see all command-line options.  The @verb{--help} list will automatically stay up to date, unlike this documentation.}
-            @item{@verb{--server <true-or-false>} -- whether to run the web server.  Defaults to false.}
-            @item{@verb{--server-port <port-number>} -- Port to use when running server.  Defaults to 8080.}
-            @item{@verb{--server-ip <ip-address>} -- Listen for connections to <ip-address>.  Use @verb{false} to accept connections from all the machine's addresses.  Defaults to @verb{127.0.0.1}.}
+            @item{@verb{--help} -- See all command-line options.  The @verb{--help} list will automatically stay up to date, unlike this documentation.}
+            @; TODO - the version option should be able to print the xsmith version AND the version of a fuzzer made using xsmith.  There should be an argument for giving a program version.
+            @item{@verb{--version} -- Prints the version info of Xsmith and exits.}
+            @item{@verb{--server <true-or-false>} -- Whether to run the web server.  Defaults to @verb{false}.}
+            @item{@verb{--server-port <port-number>} -- Port to use when running server.  Defaults to @verb{8080}.}
+            @item{@verb{--server-ip <ip-address>} -- Listen for connections to @verb{<ip-address>}.  Use @verb{false} to accept connections from all the machine's addresses.  Defaults to @verb{127.0.0.1}.}
+            @item{@verb{--server-path <url-path>} -- Run the server at @verb{localhost:8080/url-path}.  Defaults to @verb{/}.}
             @item{@verb{--seed} -- Random seed for program generation.  Defaults to whatever Racket does to initialize randomness normally.}
             @item{@verb{--output-file <filename>} -- Outputs to <filename> instead of standard output when not used with @verb{--server}.}
             @item{@verb{--max-depth <n>} -- Maximum depth of the generated program tree.}
-            @item{@verb{--version} -- prints the version info of xsmith and exits.}
-            @; TODO - the version option should be able to print the xsmith version AND the version of a fuzzer made using xsmith.  There should be an argument for giving a program version.
-            @item{@verb{--with-<feature> <bool>} -- enables or disables generator-specific features, where @verb{<feature>} is replaced with concrete feature names.}
+            @item{@verb{--tree-on-error <true-or-false>} -- Whether to show a partial program tree when an error occurs.  Defaults to @verb{false}.}
+            @item{@verb{--with-<feature> <bool>} -- Enables or disables generator-specific features, where @verb{<feature>} is replaced with concrete feature names.}
             ])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
