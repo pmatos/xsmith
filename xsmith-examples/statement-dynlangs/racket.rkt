87f757f08fb0496891a29579dcf17d0846e6f16b
William G Hatch <william@hatch.uno>
2020-03-31 16:18:49 -0600

    statement-dynlangs - add a racket fuzzer that's probably more featureful than schemely

---
+++ xsmith-examples/statement-dynlangs/racket.rkt
@@ -0,0 +1,222 @@
+#lang racket/base
+
+(require
+ xsmith
+ racr
+ xsmith/racr-convenience
+ "core.rkt"
+ racket/string
+ racket/list
+ racket/pretty
+ )
+
+(define-spec-component racket-comp)
+
+(add-basic-expressions racket-comp
+                       #:Program #t
+                       #:AssignmentExpression #t
+                       #:IfExpression #t
+                       #:ExpressionSequence #t
+                       #:LetSequential #t
+                       #:LambdaWithExpression #t
+                       #:Booleans #t
+                       #:Strings #t
+                       #:ImmutableList #t
+                       #:MutableArray #t
+                       #:MutableArrayAssignmentExpression #t
+                       #:ImmutableArray #t
+                       #:MutableStructuralRecord #t
+                       #:MutableStructuralRecordAssignmentExpression #t
+                       #:ImmutableStructuralRecord #t
+                       )
+
+
+
+(add-prop
+ racket-comp
+ render-hole-info
+ [#f (λ (h) '_HOLE_)])
+
+
+(define (render-child sym n)
+  (render-node (ast-child sym n)))
+(define (render-children sym n)
+  (map render-node (ast-children (ast-child sym n))))
+(define ((binary-op-renderer op) n)
+  `(,op ,(render-child 'l n) ,(render-child 'r n)))
+
+(add-prop
+ racket-comp
+ render-node-info
+
+ [Program (λ (n)
+            `((define (safe-divide numerator divisor)
+                (if (equal? 0 divisor)
+                    0
+                    (/ numerator divisor)))
+              (define (safe-car list fallback)
+                (if (null? list)
+                    fallback
+                    (car list)))
+              (define (safe-cdr list fallback)
+                (if (null? list)
+                    fallback
+                    (cdr list)))
+              ,@(render-children 'definitions n)
+              ,(render-child 'Expression n)))]
+
+ [Definition (λ (n)
+               `(define ,(ast-child 'name n)
+                  ,(render-child 'Expression n)))]
+ [AssignmentExpression
+  (λ (n) `(set! ,(ast-child 'name n) ,(render-child 'newvalue n)))]
+ [ExpressionSequence
+  (λ (n)
+    `(begin
+       ,@(render-children 'effectexpressions n)
+       ,(render-child 'finalexpression n)))]
+ [LetSequential
+  (λ (n)
+    `(let* (,@(map (λ (dn) `[,(ast-child 'name dn)
+                             ,(render-child 'Expression dn)])
+                   (ast-children (ast-child 'definitions))))
+       ,(render-child 'Expression n)))]
+
+ [IfExpression
+  (λ (n)
+    `(if ,(render-child 'test n)
+         ,(render-child 'then n)
+         ,(render-child 'else n)))]
+
+ [VariableReference (λ (n) (ast-child 'name n))]
+
+ [ProcedureApplication
+  (λ (n)
+    `(#%app ,(render-child 'procedure n)
+            ,@(render-children 'arguments n)))]
+ [FormalParameter (λ (n) (ast-child 'name n))]
+ [LambdaWithExpression
+  (λ (n)
+    `(lambda (,@(render-children 'parameters n))
+       ,(render-child 'body n)))]
+
+ [BoolLiteral (λ (n) (not (not (ast-child 'v n))))]
+ [Not (λ (n) `(not ,(render-child 'Expression n)))]
+ [And (binary-op-renderer 'and)]
+ [Or (binary-op-renderer 'or)]
+
+ [IntLiteral (λ (n) (ast-child 'v n))]
+ [Plus (binary-op-renderer '+)]
+ [Minus (binary-op-renderer '-)]
+ [Times (binary-op-renderer '*)]
+ [LessThan (binary-op-renderer '<)]
+ [GreaterThan (binary-op-renderer '>)]
+
+ [SafeDivide (binary-op-renderer 'safe-divide)]
+
+ [StringLiteral (λ (n) (ast-child 'v n))]
+ [StringAppend (binary-op-renderer 'string-append)]
+ [StringLength (λ (n) `(string-length ,(render-child 'Expression n)))]
+
+ [ImmutableListLiteral
+  (λ (n) `(list ,@(render-children 'expressions n)))]
+ [ImmutableListSafeCar
+  (λ (n) `(safe-car ,(render-child 'list n)
+                    ,(render-child 'fallback n)))]
+ [ImmutableListSafeCdr
+  (λ (n) `(safe-cdr ,(render-child 'list n)
+                    ,(render-child 'fallback n)))]
+ [ImmutableListCons
+  (λ (n) `(cons ,(render-child 'newvalue n)
+                ,(render-child 'list n)))]
+ [MutableArrayLiteral
+  (λ (n) `(vector ,@(render-children 'expressions n)))]
+ [ImmutableArrayLiteral
+  (λ (n) `(vector-immutable ,@(render-children 'expressions n)))]
+ [MutableArrayReference
+  (λ (n)
+    (define array-rendered (render-child 'array n))
+    `(vector-ref ,array-rendered
+                 (modulo ,(render-child 'index n)
+                         (vector-length ,array-rendered))))]
+ [ImmutableArrayReference
+  (λ (n)
+    `(let ([vec ,(render-child 'array n)])
+       (vector-ref vec
+                   (modulo ,(render-child 'index n)
+                           (vector-length vec)))))]
+ [MutableArrayAssignmentExpression
+  (λ (n)
+    (define array-rendered (render-child 'array n))
+    `(vector-set! ,array-rendered
+                  (modulo ,(render-child 'index n)
+                          (vector-length ,array-rendered))
+                  ,(render-child 'newvalue n)))]
+ [ImmutableArraySet
+  (λ (n)
+    (define array-rendered (render-child 'array n))
+    `(let ([vec ,(render-child 'array n)])
+       (vector-set vec
+                   (modulo ,(render-child 'index n)
+                           (vector-length vec))
+                   ,(render-child 'newvalue n))))]
+
+ [MutableStructuralRecordLiteral
+  (λ (n)
+    `(make-hash ,@(map (λ (name val)
+                         `(cons ',name
+                                ,(render-node val)))
+                       (ast-child 'fieldnames n)
+                       (ast-children (ast-child 'expressions n)))))]
+ [ImmutableStructuralRecordLiteral
+  (λ (n)
+    `(hash ,@(flatten (map (λ (name val)
+                             `(',name
+                               ,(render-node val)))
+                           (ast-child 'fieldnames n)
+                           (ast-children (ast-child 'expressions n))))))]
+ [MutableStructuralRecordReference
+  (λ (n)
+    `(hash-ref ,(render-child 'record n)
+               ',(ast-child 'fieldname n)))]
+ [ImmutableStructuralRecordReference
+  (λ (n)
+    `(hash-ref ,(render-child 'record n)
+               ',(ast-child 'fieldname n)))]
+ [MutableStructuralRecordAssignmentExpression
+  (λ (n)
+    `(hash-set! ,(render-child 'record n)
+                ',(ast-child 'fieldname n)
+                ,(render-child 'newvalue n)))]
+ [ImmutableStructuralRecordSet
+  (λ (n)
+    `(hash-set ,(render-child 'record n)
+               ',(ast-child 'fieldname n)
+               ,(render-child 'newvalue n)))]
+ )
+
+
+
+(assemble-spec-components
+ racket
+ racket-comp)
+
+
+(define (racket-generate)
+  (parameterize ([current-xsmith-type-constructor-thunks
+                  (type-thunks-for-concretization)])
+    (racket-generate-ast 'Program)))
+
+(define (racket-format-render s-exps)
+  (define out (open-output-string))
+  (for ([symex s-exps])
+    (pretty-print symex out 1))
+  (get-output-string out))
+
+(module+ main
+  (xsmith-command-line
+   racket-generate
+   #:format-render racket-format-render
+   #:comment-wrap (λ (lines) (string-join (map (λ (l) (format ";; ~a" l)) lines)
+                                          "\n"))
+   ))
