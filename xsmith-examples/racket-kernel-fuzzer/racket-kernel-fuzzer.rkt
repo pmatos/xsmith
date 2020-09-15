4edc2972549b896c05bcd2af00dc92514b29a305
William G Hatch <william@hatch.uno>
2020-09-15 11:17:36 -0600

    racket kernel fuzzer: add NE/atan to printer

--- xsmith-examples/racket-kernel-fuzzer/racket-kernel-fuzzer.rkt
+++ xsmith-examples/racket-kernel-fuzzer/racket-kernel-fuzzer.rkt
@@ -565,7 +565,17 @@
                   (/ arg1))
               (apply / arg1 (map (λ (x) (if (eq? 0 x) 1 x)) args)))))
       ;; TODO - add safe/NoException wrappers
-      ;; TODO - NE/atan
+      (define-values (NE/atan)
+        (λ (x [y 'no])
+          (if (eq? y 'no)
+              (if (or (equal? x 0+1i)
+                      (equal? x 0-1i))
+                  0
+                  (atan x))
+              (if (or (equal? x 0)
+                      (equal? y 0))
+                  0
+                  (atan x y)))))
       (define-values (safe-car)
         (λ (list fallback)
           (if (null? list)
